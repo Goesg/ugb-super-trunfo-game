0d6f0644ebf7647db45fa2e1bb93cc0a51a4d1f9
Goesg <diegogoessg@gmail.com>
2025-03-30 14:03:43 -0300

    Construtor de displays, redimensionamento de display em tempo real

---
+++ src/Display/Enums/RegiaoTabuleiro.cs
@@ -0,0 +1,58 @@
+namespace Display.Enum
+{
+
+    public enum RegiaoTabuleiro
+    {
+        AreaDoJogo,
+        AreaDoInput
+    }
+
+    public static class RegiaoTabuleiroEnumUtil
+    {
+        public static CoordenadasConsole GerarCoordenadasEmTempoReal(this RegiaoTabuleiro regiao)
+        {
+            return regiao switch
+            {
+                RegiaoTabuleiro.AreaDoJogo => GerarCoordernadasAreaDoJogo(),
+                RegiaoTabuleiro.AreaDoInput => GerarCoordenadasAreaInput(),
+                _ => throw new Exception($"Coordernadas da Regiao - {regiao.ToString()} não encontrada")
+            };
+        }
+
+        internal static int ObterLarguraConsole()
+        {
+            return Console.WindowWidth;
+        }
+
+        internal static int ObterAlturaConsole()
+        {
+            return Console.WindowHeight;
+        }
+
+        //criar classe geradora de coordernadas
+
+        internal static CoordenadasConsole GerarCoordernadasAreaDoJogo()
+        {
+            return new CoordenadasConsole
+            .CoordenadasConsoleBuilder()
+            .ComEspacosAEsqueda(0)
+            .ComEspacosDoTopo(0)
+            .ComLargura(ObterLarguraConsole())
+            .ComAltura(ObterAlturaConsole() - 6)
+            .Build();
+        }
+
+        internal static CoordenadasConsole GerarCoordenadasAreaInput()
+        {
+            return new CoordenadasConsole
+            .CoordenadasConsoleBuilder()
+            .ComEspacosAEsqueda(0)
+            .ComEspacosDoTopo(ObterAlturaConsole() - 9)
+            .ComLargura(ObterLarguraConsole())
+            .ComAltura(6)
+            .Build();
+        }
+    }
+
+}
+
